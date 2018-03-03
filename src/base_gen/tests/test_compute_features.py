419d7772fc4d0b8563656decc3998b55eecf0a10
Bartosz Górski <bartoszg16@gmail.com>
2018-03-03 16:21:06 +0100

    Fix tests

--- src/base_gen/tests/test_compute_features.py
+++ src/base_gen/tests/test_compute_features.py
@@ -1,29 +1,23 @@
 from src.base_gen.BaseGen import BaseGen
 
-EN_SENTENCE = """
-That guy run like a jaguar-rabbit... 
-Man was like 70 years old but could do a back flip, drop into the splits, and come up running. 
-He's awesome.
- """
-PL_SENTENCE = """
-Koleś był jak jaguaro-zając...
-Miał 70 lat, ale potrafił zrobić salto do tyłu, szpagat, i nadal biec.
-Jest świetny.
-"""
+SIMPLE_SENTENCE = "Abćńa I'm 125 xz. Ablss, 5% oo-k3 qw-op ok Lol"
 
 
 class TestCalculateFeatures:
     def setup_class(self):
         self.base_gen = BaseGen()
+        self.words = self.base_gen.extract_words(SIMPLE_SENTENCE)
+
+    def test_extract_words(self):
+        assert len(self.base_gen.extract_words(SIMPLE_SENTENCE)) == 10
 
     def test_average_word_length(self):
-        assert self.base_gen.average_word_length(EN_SENTENCE) == 0
-        assert self.base_gen.average_word_length(PL_SENTENCE) == 0
+        assert self.base_gen.average_word_length(self.words) == 2.5
 
     def test_vowel_ratio(self):
-        assert self.base_gen.vowel_ratio(EN_SENTENCE) == 0
-        assert self.base_gen.vowel_ratio(PL_SENTENCE) == 0
+        sentence_vowel_ratio, word_vowel_ratio = self.base_gen.vowel_ratio(self.words)
+        assert sentence_vowel_ratio == 0.36
+        assert word_vowel_ratio == 0.9
 
     def test_average_words_in_sentences(self):
-        assert self.base_gen.average_words_in_sentences(EN_SENTENCE) == 0
-        assert self.base_gen.average_words_in_sentences(PL_SENTENCE) == 0
+        assert self.base_gen.average_words_in_sentences(len(self.words), SIMPLE_SENTENCE) == 5.0
