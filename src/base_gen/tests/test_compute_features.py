df8209cd486f5fb0645acf4269be174b253b6570
Bartosz Górski <bartoszg16@gmail.com>
2018-03-03 21:09:16 +0100

    Simplify tests

--- src/base_gen/tests/test_compute_features.py
+++ src/base_gen/tests/test_compute_features.py
@@ -12,25 +12,25 @@ class TestCalculateFeatures:
         assert len(self.base_gen.extract_words(SIMPLE_SENTENCE)) == 10
 
     def test_average_word_length(self):
-        assert self.base_gen.average_word_length(self.words) == 2.5
+        assert self.base_gen.average_word_length(self.words) == 25 / 10
 
     def test_vowel_ratio(self):
         text_vowel_ratio, word_vowel_ratio = self.base_gen.vowel_ratio(self.words)
-        assert text_vowel_ratio == 0.36
-        assert word_vowel_ratio == 0.9
+        assert text_vowel_ratio == 9 / 25
+        assert word_vowel_ratio == 9 / 10
 
     def test_average_words_in_sentences(self):
-        assert self.base_gen.average_words_in_sentences(len(self.words), SIMPLE_SENTENCE) == 5.0
+        assert self.base_gen.average_words_in_sentences(len(self.words), SIMPLE_SENTENCE) == 10 / 2
 
     def test_non_ascii_ratio(self):
-        assert self.base_gen.non_ascii_ratio(SIMPLE_SENTENCE) == 0.125
+        assert self.base_gen.non_ascii_ratio(SIMPLE_SENTENCE) == 6 / len(SIMPLE_SENTENCE)
 
     def test_double_letters_and_vowels(self):
         doubles = self.base_gen.double_letter_and_vowels_ratio(self.words)
-        assert doubles['text_doubles_letters'] == 0.12
-        assert doubles['text_doubles_vowels'] == 0.04
-        assert doubles['word_doubles_letters'] == 0.3
-        assert doubles['word_doubles_vowels'] == 0.1
+        assert doubles['text_doubles_letters'] == 3 / 25
+        assert doubles['text_doubles_vowels']  == 1 / 25
+        assert doubles['word_doubles_letters'] == 3 / 10
+        assert doubles['word_doubles_vowels']  == 1 / 10
 
     def test_alphabet_ratio(self):
         text_letter_ratio, word_letter_ratio = self.base_gen.alphabet_ratio(self.words)
