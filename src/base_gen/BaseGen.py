b74954e377d78d05f7d08e020bd1a099ab1096e0
Bartosz Górski <bartoszg16@gmail.com>
2018-03-03 16:20:45 +0100

    Change way how to calculate vowels ratio

--- src/base_gen/BaseGen.py
+++ src/base_gen/BaseGen.py
@@ -31,13 +31,28 @@ class BaseGen:
         vowel_ratio = self.__vowel_ratio(text)
         average_words_in_sentences = self.__average_words_in_sentences(text)
 
-    def average_word_length(self, text):
-        words = text.split()
+    def extract_words(self, text):
+        tokens = nltk.wordpunct_tokenize(text)
+        return [token for token in tokens if not re.search('[\W^\d]', token)]
+
+    def average_word_length(self, words):
         words_len = len(words)
         return sum(len(word) for word in words) / words_len if words_len else 0
 
-    def vowel_ratio(self, text):
-        return len(re.findall('[{}]'.format(VOWELS), text)) / len(text)
+    def vowel_ratio(self, words):
+        vowels_count = 0
+        for word in words:
+            vowels_count += len(re.findall('[{}]'.format(VOWELS), word))
+        sentence_vowel_ratio = self.__vowel_ratio_in_sentence(words, vowels_count)
+        word_vowel_ratio = self.__vowel_ratio_per_word(len(words), vowels_count)
+        return sentence_vowel_ratio, word_vowel_ratio
 
-    def average_words_in_sentences(self, text):
-        return len(text.split()) / len(nltk.sent_tokenize(text))
+    def __vowel_ratio_in_sentence(self, words, vowels_count):
+        words_len = sum(len(word) for word in words)
+        return vowels_count / words_len
+
+    def __vowel_ratio_per_word(self, words_count, vowels_count):
+        return vowels_count / words_count
+
+    def average_words_in_sentences(self, words_len, text):
+        return words_len / len(nltk.sent_tokenize(text))
