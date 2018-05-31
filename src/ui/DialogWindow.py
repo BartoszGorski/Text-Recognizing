961e5e0b2d8425aa94c5a512870303a7a527e946
EarthWave305 <earthwave305@gmail.com>
2018-05-31 14:46:57 +0200

    Classification text Entry replaced with Text. Added error dialog windows for wrong length and lack of classifier. Colouring of wrong length

---
+++ src/ui/DialogWindow.py
@@ -0,0 +1,17 @@
+﻿import tkinter
+
+
+class DialogWindow:
+    def __init__(self, parent, minLength = 0, txt = ""):
+        top = self.top = tkinter.Toplevel(parent)
+
+        if len(txt) < 1:
+            tkinter.Label(top, text="Minimalna długość tekstu wynosi " + str(minLength) + " znaków!").pack()
+        else:
+            tkinter.Label(top, text=txt).pack()
+
+        okButton = tkinter.Button(top, text="OK", command=self.ok)
+        okButton.pack(pady=5)
+
+    def ok(self):
+        self.top.destroy()
